f5044b69b3930babd1989429dc65730b189c31ca
Will Sanford <wsanford99@gmail.com>
2021-08-25 20:19:53 -0400

    Implemented broadcasting

--- src/math/tensor_math.cpp
+++ src/math/tensor_math.cpp
@@ -1,11 +1,73 @@
 #include <cstdlib>
+#include <string>
 #include <vector>
 #include <algorithm>
 #include "tensor.h"
 #include "tensor_math.h"
 
 using std::vector;
-/* Tensors must be of the following shape
+
+
+/**
+    Broadcast the dimensions of two tensors to a destination. Casting will be different for multiplication and addition operations.
+    Broadcasting general
+        broadcasting two tensors of shape
+            t1 = a_n x a_n-1 x .. x a_2 x a_1
+            t2 = b_n x b_n-1 x .. x b_2 x b_1
+        will produce a tensor of shape
+            t3 = max(a_n, b_n) x ... x max(a_1, b_1)
+        Only three cases are valid. Either a_n and b_n are equal, or one (or both)
+        are 1, or one of them does not exist
+
+        Examples
+        t1 = 3x3x3
+        t2 = 1x3
+        t3 = 3x3x3
+
+        t1 = 3x2x3
+        t2 = 1x3x3
+        t3 = ERROR
+    Breaodcasting Multiplication
+*/
+TMATH_STATUS broadcast(Tensor *src1, Tensor *src2, vector<int> *dst, string op){
+    vector<int>*dims1 = src1->get_dims();
+    int dim1 = src1->get_num_dims();
+    vector<int>*dims2 = src2->get_dims();
+    int dim2 = src2->get_num_dims();
+    int dst_dims_n = max(dim1, dim2);
+    vector<int> dst_dims = new vector<int>(dst_dims_n);
+
+    switch(op){
+        case "add":
+            for (int i = 0; i < dst_dims_n; i++){
+                // This dimenstion does not exist in 1
+                if ((dst_dims_n - i) > dim1){  
+                    dst_dims[i] = dims2[i];
+                // This dimension does not exist in 2
+                }elif((dst_dims_n - i) > dim2){
+                    dst_dims[i] = dims1[i];
+                // The dimensions are equal
+                }elif(dims1[i] == dims2[i]){
+                    dst_dims[i] = dims1[i];
+                // The first dimension is 1, so broadcast the other
+                }elif(dims1[i] == 1){
+                    dst_dims[i] = dims2[i];
+                // The second dimention is 1, so broadcast the other
+                }elif(dims2[i] == 1){
+                    dst_dims[i] = dims1[i];
+                }else{
+                    return TMATH_FAILURE;
+                }
+
+            }
+            break;
+
+        case "mult":
+            break;
+    }
+}
+/**
+    @brief Tensors must be of the following shape
     shape(src1) = a_1 x b_1 x c_1 x ... n_1 x dim_1 x dim_2
     shape(src2) = a_2 x b_2 x c_2 x ... n_2 x dim_2 x dim_3
    Where for the x_i dimensions, they must be equal or one of them must be one or non existent. The
@@ -13,7 +75,7 @@ using std::vector;
    The output tensor will be of the following shape
     shape(dst) = max(a_1, a_2) x max(b_1, b_2) ... max(n_1, n_2) x dim_1 x dim_3
 */
-TMATH_STATUS tensor_mult(Tensor *src1, Tensor *src2, Tensor *dst){
+TMATH_STATUS tmult(Tensor *src1, Tensor *src2, Tensor *dst){
     // Dimension preprocessing 
     /* Casting types
         0 : Even dimensions
@@ -29,10 +91,37 @@ TMATH_STATUS tensor_mult(Tensor *src1, Tensor *src2, Tensor *dst){
     // int* casting = (int *) malloc()
     return TMATH_SUCCESS;
 }
-
-/*
-    Add a constant to all the elements of a tensor covered by the mast
+/**
+    Multiply every value in a tensor by
 */
+TMATH_STATUS tmult_const(Tensor *src, float add){
+    return TMATH_SUCCESS;
+} 
+/**
+    Add a single constant to all the elements of a tensor that correspond to non zero elements in the mask tensor
+*/  
+TMATH_STATUS tmult_const_mask(Tensor *src, float add, Tensor *mask){
+    return TMATH_SUCCESS;
+}
+/**
+    Add two tensors.
+*/
+TMATH_STATUS tadd(Tensor* src1, Tensor *src2){
+    return TMATH_SUCCESS
+}
+/**
+    Add a single constant to all the elements of a tensor
+*/
+TMATH_STATUS tadd_const(Tensor *src, float add){
+
+    return TMATH_SUCCESS;
+} 
+/**
+    Add a single constant to all the elements of a tensor that correspond to non zero elements in the mask tensor
+*/  
+TMATH_STATUS tadd_const_mask(Tensor *src, float add, Tensor *mask){
+    return TMATH_SUCCESS;
+}
 // TODO : Update to use std::vector
 TMATH_STATUS tensor_add(Tensor *src, int add, bool mask, Tensor *mask_val){
     // If mask if true, check that the tensor pointer is not null
--- src/math/tensor_math.h
+++ src/math/tensor_math.h
@@ -12,6 +12,13 @@ enum TMATH_STATUS{
 /*
     Math library functions
 */
+TMATH_STATUS broadcast(Tensor *src1, Tensor *src2, vector<int> *dst, string op = "add");
+TMATH_STATUS tmult(Tensor *src1, Tensor *src2, Tensor *dst);
+TMATH_STATUS tmult_const(Tensor *src, float add);
+TMATH_STATUS tmult_const_mask(Tensor *src, float add, Tensor *mask);
+TMATH_STATUS tadd(Tensor* src1, Tensor *src2);
+TMATH_STATUS tadd_const(Tensor *src, float add);
+TMATH_STATUS tadd_const_mask(Tensor *src, float add, Tensor *mask);
 TMATH_STATUS tensor_mult  (Tensor *src1, Tensor *src2, Tensor *dst);
 TMATH_STATUS tensor_add(Tensor *src, int add, bool mask, Tensor *mask_val = nullptr);
 
